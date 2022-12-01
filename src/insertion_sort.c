1d26c06f76138a18531f682a73afaba24b89d12b
Andreas-Bugge <202204219@post.au.dk>
2022-12-01 13:18:11 +0100

    Aflevering 8

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -3,35 +3,60 @@
 
 void sort(linked_list *llPtr)
 {
- struct node *future, *current, *previous, *flex;
- /*points to the first unsorted node at the start*/
- previous = llPtr->head;
- current = llPtr->head;
- future = llPtr->head->next;
-while (future != NULL)
-{
-    /*Starts sorting from the top of the list*/
-    current = llPtr->head;
-    previous = llPtr->head;
-    current = future->next;
+struct node *current, *previous, *flex;
+/*points to the first unsorted node at the start*/
+previous = llPtr->head;
+current = llPtr->head->next;
+
+while (current != NULL)
+    {
     /*sorts in case of the next unsorted being higher than the previous*/
-    while (current->data <= future->data)
-    {
-        flex->next = current->next;
-        current->next = future->next;
-        previous->next=flex->next;
-    }
-    if(current != NULL)
-    {
-        llPtr->head = future;
-        previous->next = future->next;
-        future->next = current;
-    }
-    else
-    {
-    future->next = NULL;
-    previous->next = future;
-    }
-    future = future->next;  
-}
+    if (previous->data > current->data)
+        {
+
+        if (previous == llPtr->head)
+            {
+            previous->next = current->next;
+            current->next = previous;
+            llPtr->head = current;
+            }
+
+        else
+            {
+            flex = llPtr->head;
+
+            if (current->data < flex->data)
+                {
+                previous->next = current->next;
+                current->next = flex;
+                llPtr->head = current;
+                }
+
+            else
+                {
+                while (flex->next->data <= current->data)
+                {
+                    flex = flex->next;
+                }
+           /* if (flex->next->next == current)
+                {
+                flex->next->next = current->next;
+                current->next = flex->next;
+                flex->next = current;
+                }*/
+                
+                previous->next = current->next;
+                current->next = flex->next;
+                flex->next = current;
+                }
+            }
+            current = previous->next;
+        }
+        else
+        {
+        previous = current;
+        current = previous->next;
+        }
+    }   
+    return;
 }
--- src/list_queue.c
+++ src/list_queue.c
@@ -3,26 +3,47 @@
 void init_queue(queue *q)
 {
     q->size = 0;
-    q->front = 0;
-    q->rear = 0;
+    q->front = NULL;
+    q->rear = NULL;
 }
 
 int empty(queue *q)
 {
-  return (q->front == q->rear);
+  if (q->size==0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+  
 }
 
 void enqueue(queue *q, int x)
 {
-  q->rear = x;
-  q->rear = q->rear +1;
-  q->size = q->size -1; 
+  qnode *help = (qnode *)malloc(sizeof(qnode));
+  help->data = x;
+  help->next = NULL;
+  if(empty(q) == 1)
+  {
+    q->front = help;
+  }
+  else
+  {
+    q->rear->next = help;
+  }
+  q->rear = help;
+  q->size = q->size + 1;
 }
 
 int dequeue(queue *q)
 {
-int dequeueing = (int)q->front;
-q->front = q->front+1;
-q->size = q->size +1;
+assert(empty(q) == 0);
+int dequeueing = (int)q->front->data;
+qnode *front = q->front;
+q->front = q->front->next;
+free(front);
+q->size = q->size-1;
 return dequeueing;
 }
